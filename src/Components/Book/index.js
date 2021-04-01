8cec61ad408861574ceaadd823086cb1c7ca16e5
Ananya <ananya.appan@gmail.com>
2021-04-01 18:54:23 +0530

    initial commit

---
+++ src/Components/Book/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class Book extends React.Component{
+    componentDidMount(){
+        
+    }
+
+    state = {
+        selectedFile : null,
+        text : ""
+    };
+
+    onChange = (event) => {
+        console.log("changing!")
+        this.setState({
+            selectedFile : event.target.files[0]
+        })
+    }
+
+    onUpload = () => {
+        var fr=new FileReader();
+        fr.onload = async (e) => { 
+            const text = (e.target.result)
+            console.log(text)
+            this.setState({
+                text : e.target.result
+            })
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(this.state.text,"text/xml");
+            console.log(xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue);
+          };
+        fr.readAsText(this.state.selectedFile);
+    }
+
+    render(){
+        return(
+            <div>
+                <input type="file" onChange={this.onChange} />
+                <button onClick={this.onUpload}></button>
+            </div>
+        )
+    }
+}
+
+export default Book;
+
+// x-special/nautilus-clipboard
+// copy
+// file:///home/ananya/Documents/DM/project/ranstuff/docbook/ex2.xml
+
+
