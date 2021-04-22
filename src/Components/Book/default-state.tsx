ed11f019ff6abae8447813adfe687616b1160cc9
Ananya <ananya.appan@gmail.com>
2021-04-22 14:17:34 +0530

    changed defaultState.tsx

--- src/Components/Book/default-state.tsx
+++ src/Components/Book/default-state.tsx
@@ -17,41 +17,39 @@ export const defaultState: State = {
       {
         value: 'Book Title',
         label : 'Book Title',
-        options: []
+        options: [{
+          value: 'Chapter 1',
+          label: 'Chapter 1',
+          options: [
+            {
+              value: 'Para 1',
+              label: 'Para 1',
+              options: []
+            },
+            {
+              value: 'Para 2',
+              label: 'Para 2',
+              options: []
+            }  
+          ]
+        },
+        {
+          value: 'Chapter 2',
+          label: 'Chapter 2',
+          options: [
+            {
+              value: 'Para 1',
+              label: 'Para 1',
+              options: []
+            },
+            {
+              value: 'Para 2',
+              label: 'Para 2',
+              options: []
+            }  
+          ]
+        }]
       },
-      {
-        value: 'Chapter 1',
-        label: 'Chapter 1',
-        options: [
-          {
-            value: 'Para 1',
-            label: 'Para 1',
-            options: []
-          },
-          {
-            value: 'Para 2',
-            label: 'Para 2',
-            options: []
-          }  
-        ]
-      },
-      {
-        value: 'Chapter 2',
-        label: 'Chapter 2',
-        options: [
-          {
-            value: 'Para 1',
-            label: 'Para 1',
-            options: []
-          },
-          {
-            value: 'Para 2',
-            label: 'Para 2',
-            options: []
-          }  
-        ]
-      }
-
     ]
     // options: [
     //   {
