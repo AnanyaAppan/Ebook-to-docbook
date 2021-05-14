bd921cc724b37a375e81196ccbf3c59fd0347d97
SLR1999 <lalithareddy.seelam@gmail.com>
2021-05-14 15:24:54 +0530

    merging

--- src/Components/xmlToPdf.js
+++ src/Components/xmlToPdf.js
@@ -1,21 +1,30 @@
 import React from "react";
+import exec from "child_process";
 
 export default class XmlToPdf extends React.Component {
 
     exportAsPdf = () => {
         console.log("exporting as pdf!")
-        const exec = require("child_process").exec
-        exec('ls', (err, stdout, stderr) => console.log(stdout))
+        // var exec = require("child_process").exec, child;
+        // child = exec('ls',
+        // function (error, stdout, stderr) {
+        //     console.log('stdout: ' + stdout);
+        //     console.log('stderr: ' + stderr);
+        //     if (error !== null) {
+        //          console.log('exec error: ' + error);
+        //     }
+        // });
+        // child();
 
-        // exec("ls -la", (error, stdout, stderr) => {
-        //     if (error) {
-        //         console.log(`error: ${error.message}`)
-        //     }
-        //     if (stderr) {
-        //         console.log(`stderr: ${stderr}`)
-        //     }
-        //     console.log(`stdout: ${stdout}`)
-        // });  
+        exec("ls -la", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`)
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`)
+            }
+            console.log(`stdout: ${stdout}`)
+        });  
     }
 
     
